a485460c63be17665a4158e15f887b35dcf3617d
KethmikaEdirisinghe <as20240515@sci.sjp.ac.lk>
2026-03-11 20:38:49 +0530

    Add api folder and db connect.php file

---
+++ api/db_connect.php
@@ -0,0 +1,16 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "flood_relief_db";
+
+ini_set('log_errors', 1);
+ini_set('error_log', __DIR__ . '/api_errors.log');
+error_reporting(E_ALL);
+ini_set('display_errors', 0);
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die(json_encode(["status" => "error", "message" => "Connection failed: " . $conn->connect_error]));
+}
