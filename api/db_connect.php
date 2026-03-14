8f4589eb3ec910ae7e200821a313c1921fb0ad39
KethmikaEdirisinghe <as20240515@sci.sjp.ac.lk>
2026-03-14 10:56:24 +0530

    Add login.php file fix syntax issue in db_connect.php file

--- api/db_connect.php
+++ api/db_connect.php
@@ -14,3 +14,4 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
     die(json_encode(["status" => "error", "message" => "Connection failed: " . $conn->connect_error]));
 }
+?>
